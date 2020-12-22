e0b9a1fed769388c3c777c3c20ed14907da65a9a
Maxim Vainikka <vainikka@users.noreply.github.com>
2020-12-22 18:25:42 +1000

    adding a list of teams and countries

--- GuGa/ContentView.swift
+++ GuGa/ContentView.swift
@@ -8,12 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var csgoTeams = ["Team Liquid", "Vitality", "BIG", "Virtus Pro", "Team Spirit"].shuffled()
+    @State private var selectedTeam = Int.random(in: 0...4)
+    @State private var showScore = false
+    @State private var testResult = ""
+    @State private var gameScore = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+            ZStack{
+                LinearGradient(gradient: Gradient(colors: [.pink, .black]), startPoint: /*@START_MENU_TOKEN@*/.topLeading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.bottomTrailing/*@END_MENU_TOKEN@*/).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                VStack(spacing: 30){
+                Spacer()
+                VStack{
+                    Text("The homeland of")
+                    Text("\(csgoTeams[selectedTeam]) is")
+                }.foregroundColor(Color.white)
+                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                    ForEach(0..<4) { team in
+                    Button(action: {
+                        self.flagTapped(team: team)
+                    }){
+                        Image(self.csgoTeams[team])
+                            .renderingMode(/*@START_MENU_TOKEN@*/.original/*@END_MENU_TOKEN@*/)
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.black, lineWidth: 3))
+                            .shadow(color: .black, radius: 7, x: 5.0, y: 5.0)
+                                        }
+                                }
+                Spacer()
+            }
+            }.alert(isPresented: $showScore){
+                Alert(title: Text(testResult), message: Text("Your score is \(gameScore)"), dismissButton: .default(Text("Continue")){
+                    self.wantToContinue()
+                })
+            }
+        }
+
+    func flagTapped(team: Int){
+        if team == selectedTeam{
+            testResult = "Correct"
+            gameScore+=1
+        }else{
+            testResult = "Wrong"
+            gameScore-=1
+        }
+        showScore = true
+    }
+    func wantToContinue() {
+        csgoTeams.shuffle()
+        selectedTeam = Int.random(in: 0...4)
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
